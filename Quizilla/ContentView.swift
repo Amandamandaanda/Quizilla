b1885198ff80a784267076363406d820beb505eb
Amandamandaanda <amandatanaskovic@gmail.com>
2026-03-27 14:08:18 +0100

    Initial Commit

---
+++ Quizilla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quizilla
+//
+//  Created by Amanda Tana on 2026-03-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quizilla/QuizillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizillaApp.swift
+//  Quizilla
+//
+//  Created by Amanda Tana on 2026-03-27.
+//
+
+import SwiftUI
+
+@main
+struct QuizillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
