485a00136e286077e61fdd129c7a2effa55274c7
aurevau <3kdapp25.vaudau@folkuniversitetet.nu>
2026-03-29 14:12:25 +0200

    added a button modifier so we can have the same type of button throughout project

--- Quizilla/Modifiers/ViewModifiers/ButtonModifier.swift
+++ Quizilla/Modifiers/ViewModifiers/ButtonModifier.swift
@@ -5,4 +5,21 @@
 //  Created by Familjen on 2026-03-29.
 //
 
-import Foundation
+
+import SwiftUI
+
+struct ButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundStyle(Color.theme.buttonTextColor)
+            .frame(width: 352, height: 50)
+            
+            .background(LinearGradient(colors: [Color.theme.buttonBorder.opacity(0.7), Color.theme.secondaryPurpleColor.opacity(0.5)],
+                startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(12)
+            .overlay(RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.theme.buttonBorder.opacity(0.2), lineWidth: 2))
+    }
+}
