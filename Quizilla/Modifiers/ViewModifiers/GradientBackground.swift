88b27de1081f721c88b36f0e7dc18bb1a8e853be
aurevau <3kdapp25.vaudau@folkuniversitetet.nu>
2026-03-29 14:13:49 +0200

    Added a gradient modifier to use as backgrounds throughout app

--- Quizilla/Modifiers/ViewModifiers/GradientBackground.swift
+++ Quizilla/Modifiers/ViewModifiers/GradientBackground.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct GradientBackground: ViewModifier {
+    func body(content: Content) -> some View {
+        ZStack {
+            LinearGradient(
+                colors: [
+                    Color.theme.background,
+                    Color.theme.purpleColor.opacity(0.4)
+                ],
+                               
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            content
+
+        }
+    }
+}
+
+extension View {
+    func gradientBackground() -> some View {
+        self.modifier(GradientBackground())
+    }
+}
