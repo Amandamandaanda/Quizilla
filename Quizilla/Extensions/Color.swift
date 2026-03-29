e2e6ae7fc37e1f7f04d7d01249d5c168a6fda77b
aurevau <3kdapp25.vaudau@folkuniversitetet.nu>
2026-03-29 13:15:39 +0200

    Added inital colors for theme setup, can be changed later

---
+++ Quizilla/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  Quizilla
+//
+//  Created by Familjen on 2026-03-29.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let background = Color("BackgroundColor")
+    let text = Color("TextColor")
+    let secondaryTextColor = Color("SecondaryTextColor")
+    let purpleColor = Color("PurpleColor")
+    let secondaryPurpleColor = Color("SecondaryPurpleColor")
+    let pinkColor = Color("PinkColor")
+}
