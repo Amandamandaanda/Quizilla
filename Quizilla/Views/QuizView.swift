0c0197b7f60643b8401c6843fbf8ebd8ca6b3910
Amandamandaanda <amandatanaskovic@gmail.com>
2026-04-01 15:07:46 +0200

    Connect resultview, startview and quizview to viewmodel and added back to start button in resultview.

--- Quizilla/Views/QuizView.swift
+++ Quizilla/Views/QuizView.swift
@@ -12,18 +12,14 @@ struct QuizView: View {
     
     // Placeholders for now, fetch from viewmodel later
     
-    @Binding var screen: showScreen
-    @Binding var currentQuestionIndex: Int
-    @Binding var score: Int
-
-    let questions: [Question]
+    @Bindable var viewModel: QuizViewModel
     
     @State private var selectedAnswerIndex: Int? = nil
 
     
     var body: some View {
         
-        let question = questions[currentQuestionIndex]
+        let question = viewModel.gameQuestions[viewModel.currentQuestionIndex]
 
         ZStack {
             
@@ -40,8 +36,8 @@ struct QuizView: View {
                         .modifier(CardModifier())
                         .multilineTextAlignment(.center)
                     
-                    ProgressView(value: Double(currentQuestionIndex + 1),
-                                 total: Double(questions.count))
+                    ProgressView(value: Double(viewModel.currentQuestionIndex + 1),
+                                 total: Double(viewModel.gameQuestions.count))
                         .progressViewStyle(.linear)
                         .padding(.horizontal, 30)
                         .padding(.vertical)
@@ -50,64 +46,27 @@ struct QuizView: View {
                         ForEach(question.options.indices, id: \.self) {index in
                             
                             Button {
-                                submitAnswer(index: index)
+                                viewModel.submitAnswer(index)
                             } label: {
                                 Text(question.options[index])
                                     .frame(maxWidth: .infinity)
                                     .padding()
                                     .background(backgroundColor(for: index))
-                                    
                             }
                             .modifier(ButtonModifier())
-                            .disabled(selectedAnswerIndex != nil)
+                            .disabled(viewModel.selectedAnswerIndex != nil)
                         }
-                
                 }
-
-                
-               
-                
-                
-                
-                
-                
             }
             .padding(.vertical, 50)
-            
-           
-            
         }
         .gradientBackground()
-        
-        
-        
     }
-    
-    
-    func submitAnswer(index: Int) {
-        let question = questions[currentQuestionIndex]
-        
-        selectedAnswerIndex = index
-        
-        if index == question.correctAnswerIndex {
-            score += 1
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            if currentQuestionIndex < questions.count - 1 {
-                currentQuestionIndex += 1
-            } else {
-                screen = .result
-            }
-            
-            selectedAnswerIndex = nil
-        }
-    }
-    
+ 
     func backgroundColor(for index: Int) -> Color {
-        let question = questions[currentQuestionIndex]
+        let question = viewModel.gameQuestions[viewModel.currentQuestionIndex]
         
-        if let selected = selectedAnswerIndex {
+        if let selected = viewModel.selectedAnswerIndex {
             if index == question.correctAnswerIndex {
                 return .green
             } else if index == selected {
