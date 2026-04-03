87550e61ce6dbe17c47609434976d3e402832c4c
jonathan strid <pjstrid@outlook.com>
2026-04-03 10:39:26 +0100

    Code clean up

--- Quizilla/Views/ContentView.swift
+++ Quizilla/Views/ContentView.swift
@@ -11,22 +11,21 @@ enum showScreen {
     case start
     case quiz
     case result
-
 }
 
 struct ContentView: View {
 
     @State private var viewModel = QuizViewModel()
-    
+
     var body: some View {
 
         switch viewModel.screen {
 
         case .start:
             StartView(viewModel: viewModel)
-                
+
         case .quiz:
-           QuizView(viewModel: viewModel)
+            QuizView(viewModel: viewModel)
 
         case .result:
             ResultView(viewModel: viewModel)
--- Quizilla/Views/ResultView.swift
+++ Quizilla/Views/ResultView.swift
@@ -7,49 +7,56 @@
 
 import SwiftUI
 
-
-
 struct ResultView: View {
     @Bindable var viewModel: QuizViewModel
-    
+
     var body: some View {
-        ZStack{
+        ZStack {
             VStack(spacing: 20) {
-                Image(systemName: viewModel.score > viewModel.gameQuestions.count / 2 ? "star.hexagon.fill" : "xmark.circle.fill")
-                    .font(.system(size: 60))
-                    .foregroundStyle(viewModel.score > viewModel.gameQuestions.count / 2 ? .green : .red)
-                            Text("Resultat")
-                                .font(.largeTitle)
-                                .fontWeight(.heavy)
-                                .foregroundStyle(Color.theme.text)
-                
+                Image(
+                    systemName: viewModel.score > viewModel.gameQuestions.count
+                        / 2 ? "star.hexagon.fill" : "xmark.circle.fill"
+                )
+                .font(.system(size: 60))
+                .foregroundStyle(
+                    viewModel.score > viewModel.gameQuestions.count / 2
+                        ? .green : .red
+                )
+                Text("Resultat")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(Color.theme.text)
+
                 if !viewModel.nickname.isEmpty {
-                    Text(viewModel.score > viewModel.gameQuestions.count / 2 ? "Bra spelat \(viewModel.nickname)!" : "Du kan bättre \(viewModel.nickname)!")
+                    Text(
+                        viewModel.score > viewModel.gameQuestions.count / 2
+                            ? "Bra spelat \(viewModel.nickname)!"
+                            : "Du kan bättre \(viewModel.nickname)!"
+                    )
                 }
-                
-                
 
-                Text("\(viewModel.score) av \(viewModel.gameQuestions.count) rätt")
-                                .font(.title2)
-                                .foregroundStyle(Color.theme.text)
-                                .padding(.bottom, 60)
+                Text(
+                    "\(viewModel.score) av \(viewModel.gameQuestions.count) rätt"
+                )
+                .font(.title2)
+                .foregroundStyle(Color.theme.text)
+                .padding(.bottom, 60)
 
-                            Button("Spela igen") {
-                                viewModel.startNewGame()
-                                
-                            }
-                            .modifier(ButtonModifier())
-                Button("Tillbaka till start") {
-                    viewModel.backToStart()
-                    
+                Button("Spela igen") {
+                    viewModel.startNewGame()
+
                 }
                 .modifier(ButtonModifier())
-                        }
-                        .padding()
-                    
+                Button("Tillbaka till start") {
+                    viewModel.backToStart()
+
+                }
+                .modifier(ButtonModifier())
+            }
+            .padding()
+
         }
-            .gradientBackground()
-  
+        .gradientBackground()
+
     }
 }
-
